d7e0658d003ac3eb57f2476511876f0f165651b7
Gabriel Lima <gabriel58221@gmail.com>
2023-07-31 17:06:09 -0300

    first commit

---
+++ src/server.js
@@ -0,0 +1,8 @@
+require("express-async-errors");
+
+const express = require("express");
+const app = express();
+
+const PORT = 3333;
+
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
